ba7367b1e0f10468c758829a61e88b8889e2c71b
Herry <65188659+HerryTS@users.noreply.github.com>
2024-12-01 15:27:35 +0100

    venv e requirements
    controllo e creazione del venv + controllo e download dei requirements

--- graboid.py
+++ graboid.py
@@ -5,6 +5,57 @@ import os
 import sys
 import subprocess
 
+# -------------------------------------- DA INTEGRARE --------------------------------------
+def requirements():
+    """Funzione per controllare l'esistenza del 
+    file dei requirements e del loro eventuale download"""
+    
+    file = "requirements.txt"
+    cartella = "project"
+    if file in os.listdir(cartella):
+        download_requirements = input(bcolors.WARNING+f"Scarico i requirements presenti in {file}? (Y/n)"+bcolors.RESET) or "Y"
+        if download_requirements.lower() == "y":
+            try:
+                subprocess.run("")
+                logging.info("Requirements scaricati con successo.")
+                return True
+            except Exception as e:
+                logging.error(f"Errore durante il download dei requirements: {e}")
+                return False
+        else:
+            return False
+    else:
+        logging.critical(f"Non ho trovato il file {file} per scaricare i requirements.\nControlla che sia nella cartella {cartella} e che abbia il nome giusto.")
+
+def check_for_venv():
+    """Funzione per controllare l'esistenza del 
+       virtual environment e della sua eventuale 
+       creazione"""
+    
+    venv = "venv"
+    cartella = "project"
+    # Cerco il virtual enviroment nella cartella del progetto
+    if venv in os.listdir(cartella): 
+        return True
+    else:
+        # Prompt per l'user
+        create_venv = input(bcolors.WARNING+f"Non è stato rilevato nessun virtual environment ({venv}). Vuoi crearne uno adesso? (Y/n)"+bcolors.RESET) or "Y"
+        
+        if create_venv.lower() == "y":
+            try:
+                # Creazione del virtual environment
+                subprocess.run(f"cd {cartella} & python -m venv {venv}")
+                logging.info("Virtual environment creato con successo")
+                return True
+            
+            except Exception as e:
+                logging.error(f"Errore durante la creazione del virtual environment: {e}")
+                return False
+            
+        else:
+            return False
+# -------------------------------------- DA INTEGRARE --------------------------------------
+
 def execute_subprocess(command: list):
     try:
         subprocess.run(command, check=True)
@@ -40,18 +91,21 @@ def process_starter(directory: str, script_path: str):
     execute_subprocess([python_executable, script_path])
     
 def webserver():
+    """Avvio del webserver con la flag -w"""
     print(bcolors.GREEN + "Starting web server ..." + bcolors.RESET)
     directory = os.path.join("project", "webapp")  # Webserver path
     script_path = os.path.join(directory, "run.py")  # Run Path
     process_starter(directory, script_path)  # Start Process
 
 def parser():
+    """Avvio del parser con la flag -p"""
     print(bcolors.GREEN + "Starting parser ..." + bcolors.RESET)
     directory = os.path.join("workspace", "experiments")  # Parser path
     script_path = os.path.join(directory, "download-parsing", "parser.py")  # Run Path
     process_starter(directory, script_path)  # Start Process
 
 def help():
+    """Messaggio per la visualizzazione della pagina di help"""
     msg = """
 +------------- Help Page --------------+
 |  syntax: python graboid.py -[h,w,p]  |
@@ -62,6 +116,7 @@ def help():
     print(msg)
         
 def error(flag) -> callable:
+    """Messaggio di errore nel caso venga inserita una flag sbagliata"""
     logging.error(f"La flag \'{flag}\' non è supportata.")
     logging.info("Consulta la guida tramite il comando: \'python graboid.py -h\'.")
     help()
@@ -69,6 +124,7 @@ def error(flag) -> callable:
 EXPECTED_DIR = 'gestione-info'
 CURRENT_DIR = os.path.basename(os.path.abspath(os.getcwd()))
 
+# Dizionario delle flag
 STARTER = {
     "-w" : webserver,
     "-p" : parser,
@@ -76,11 +132,13 @@ STARTER = {
 }
 
 if __name__ == '__main__':
-    
+
+    # Controlliamo di aver eseguito lo script nella cartella giusta    
     if CURRENT_DIR != EXPECTED_DIR:
         logging.critical(f'Esegui lo script stando nella directory indicata: \'{EXPECTED_DIR}\'.')
         sys.exit(1)
     
+    # Controllo ed esecuzione delle flag
     if len(sys.argv) > 1:
         flag = sys.argv[1]
         STARTER.get(flag, lambda: error(flag))()
